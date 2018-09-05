a8b15f0c942d8edc9985c80d3fc6c8cbbeb0a929
Naoki Tomita <tomita@cpan.org>
2018-09-06 00:33:29 +0900

    update for 8.0.1

--- KotmoguDebuff.lua
+++ KotmoguDebuff.lua
@@ -1,10 +1,7 @@
 LoadAddOn("Blizzard_CompactRaidFrames")
 
 local orbIDs = {
-    -- blue instead of cyan
-    -- [121164] = { color = "Blue",   r = 0.0039, g = 0.8745, b = 0.8431, texture = "Interface\\MiniMap\\TempleofKotmogu_ball_cyan.blp" },
     [121164] = { color = "Blue",   r = 0.0029, g = 0.6745, b = 1     , texture = "Interface\\MiniMap\\TempleofKotmogu_ball_cyan.blp" },
-    
     [121175] = { color = "Purple", r = 0.7490, g = 0     , b = 1     , texture = "Interface\\MiniMap\\TempleofKotmogu_ball_purple.blp" },
     [121176] = { color = "Green",  r = 0.0039, g = 0.8745, b = 0.0039, texture = "Interface\\MiniMap\\TempleofKotmogu_ball_green.blp" },
     [121177] = { color = "Orange", r = 1     , g = 0.5019, b = 0     , texture = "Interface\\MiniMap\\TempleofKotmogu_ball_orange.blp" },
@@ -17,7 +14,7 @@ hooksecurefunc("CompactUnitFrame_HideAllDebuffs", function(frame)
 end)
 
 hooksecurefunc("CompactUnitFrame_UpdateDebuffs", function(frame)
-    if (not UnitInBattleground("player") or not UnitIsPlayer(frame.displayedUnit) or not frame.optionTable.displayDebuffs) then
+    if (not UnitIsPlayer(frame.displayedUnit) or not frame.optionTable.displayDebuffs) then
         if frame.KotmoguDebuff then
             frame.KotmoguDebuff:Hide()
         end
@@ -47,7 +44,10 @@ hooksecurefunc("CompactUnitFrame_UpdateDebuffs", function(frame)
     end
     
     -- if unit has orb
-    local _, _, _, _, _, _, _, _, _, _, spellId, _, _, _, _, val2 = UnitDebuff(frame.displayedUnit, "Orb of Power")
+    local _, _, _, _, _, _, _, _, _, spellId, _, _, _, _, _, _, val2 = AuraUtil.FindAuraByName("Orb of Power", frame.displayedUnit, 'HARMFUL')
+
+    --    spellId, val2 = 121176, 120 -- debug
+
     if not spellId then
         orbframe:Hide()
         return
@@ -60,7 +60,7 @@ hooksecurefunc("CompactUnitFrame_UpdateDebuffs", function(frame)
     end
 
     -- set size
-    local baseSize = frame:GetHeight() * 0.9
+    local baseSize = frame:GetHeight() * 0.7
     if baseSize <= 0 then
         return
     else
@@ -70,8 +70,7 @@ hooksecurefunc("CompactUnitFrame_UpdateDebuffs", function(frame)
     end
 
     -- display
-    orbframe.debuff:SetTextColor(orb.r, orb.g, orb.b, 1)
+    orbframe.debuff:SetTextColor(1, 1, 1, 1)
     orbframe.debuff:SetText(val2)
-    -- orbframe.texture:SetTexture(orb.texture)
     orbframe:Show()
 end)
